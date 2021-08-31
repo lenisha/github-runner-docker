7eaa044e2cc3255d0693984c5b95ca6c83c307ba
Elena Neroslavskaya <lenisha@gmail.com>
2021-08-31 18:19:20 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST  -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
